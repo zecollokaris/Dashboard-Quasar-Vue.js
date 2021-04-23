6d816dd0739dadb0a8e8a5906972878788626d91
zecollokaris <collo.kariss@gmail.com>
2021-04-23 17:10:24 +0300

    added navigation menu

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,141 +1,146 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
-      <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="leftDrawerOpen = !leftDrawerOpen"
-        />
+    <q-layout view="lHh lpr lFf" class="shadow-2 rounded-borders">
 
-        <q-toolbar-title>
-          Quasar App
-        </q-toolbar-title>
+              <q-header bordered class="bg-white text-primary">
+                <!-- MENU BAR -->
+                <!-- Menu Bar Containing (Search, Live, Language Menu, Notificaton & User) -->
+                <q-toolbar class="Navigation-Menu">
+                  
+                  <q-toolbar-title>
 
-        <div>Quasar v{{ $q.version }}</div>
-      </q-toolbar>
-    </q-header>
+                    <q-avatar>
+                      <img src="https://cdn.quasar.dev/logo/svg/quasar-logo.svg">
+                    </q-avatar>
+                    Quasar Framework
+                  </q-toolbar-title>
+                  <q-btn
+                    flat
+                    dense
+                    round
+                    icon="menu"
+                    aria-label="Menu"
+                    @click="leftDrawerOpen = !leftDrawerOpen"/>
 
-    <!-- DRAWER -->
-    <!-- Drawer Containing Side Bar -->
-    <q-drawer
-      class="Bar"
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-      content-class="bg-grey-1">
-      
-      <!-- SIDE BAR -->
-      <!-- This Is The Side Bar Menu Containing Menu Options -->
-      <q-list class="Sidebar">
+                </q-toolbar>
 
-        <!-- Title Test -->
-        <q-item-label
-          header
-          class="Test">
-          Test
-        </q-item-label>
+              </q-header>
 
-      
-      <!-- PAYMENT SECTION -->
-      <!-- Payment Item Expansion Item -->
-      <q-expansion-item
-        class="Title-Payments"
-        label="Payments"
-        default-opened>
-        
+    
 
-        <q-card class="Card-Payments">
-          <q-card-section>
+            <!-- DRAWER -->
+            <!-- Drawer Containing Side Bar -->
+            <q-drawer
+              class="Bar"
+              v-model="leftDrawerOpen"
+              show-if-above
+              bordered
+              content-class="bg-grey-1">
+              
+              <!-- SIDE BAR -->
+              <!-- This Is The Side Bar Menu Containing Menu Options -->
+              <q-list class="Sidebar">
 
-            <!-- Payment Section Links -->
-            <!-- Contains Links in Payment Section -->
-            <EssentialLink
-              class="Group-3"
-              v-for="link in essentialLinks"
-              :key="link.title"
-              v-bind="link"/>
-            
-          </q-card-section>
-        </q-card>
+                <!-- Title Test -->
+                <q-item-label
+                  header
+                  class="Test">
+                  Test
+                </q-item-label>
 
-      </q-expansion-item>
-        <!-- COLLAPSE PAYMENT SECTION -->
+                  
+                <!-- PAYMENT SECTION -->
+                <!-- Payment Expansion Item -->
+                <q-expansion-item
+                    class="Title-Payments"
+                    label="Payments"
+                    default-opened>
+                    
+                    <q-card class="Card-Payments">
 
+                        <q-card-section>
+                          <!-- Payment Section Links -->
+                          <!-- Contains Links in Payment Section -->
+                          <EssentialLink
+                            class="Group-3"
+                            v-for="link in essentialLinks"
+                            :key="link.title"
+                            v-bind="link"/>
+                        </q-card-section>
 
-      <!-- Border Line Above Commerce -->
-      <div class="Line-2"></div>
+                    </q-card>
 
-      <!-- COMMERCE SECTION -->
-      <!-- Payment Item Expansion Item -->
-      
-      <q-expansion-item
-        class="Title-Commerce"
-        label="Commerce"
-        default-opened>
-        
-
-        <q-card class="Card-Commerce" >
-          <q-card-section >
-        
-        <!-- Border Line Below Commerce -->
-        <div class="Line-3"></div>
-        
-
-        <!-- Commerce Menu Items -->
-        <div class="Commerce">
-
-            <!-- Refferrals Menu-->
-            <q-item to="/referrals" clickable v-ripple>
-              <q-item-section avatar>
-                <q-icon name="group_work" />
-              </q-item-section>
-
-              <q-item-section>Referrals</q-item-section>
-            </q-item>
+                </q-expansion-item>
+                  <!-- COLLAPSE PAYMENT SECTION -->
 
 
-            <!-- Audit Logs Menu-->
-            <q-item to="/audit log" clickable v-ripple>
-              <q-item-section avatar>
-                <q-icon name="visibility" />
-              </q-item-section>
-
-              <q-item-section>Audit logs</q-item-section>
-            </q-item>
+                <!-- Border Line Above Commerce -->
+                <div class="Line-2"></div>
 
 
-            <!-- Settings Menu-->
-            <q-item to="/setting" clickable v-ripple>
-              <q-item-section avatar>
-                <q-icon name="settings" />
-              </q-item-section>
+                <!-- COMMERCE SECTION -->
+                <!-- Commerce Expansion Item -->
+                <q-expansion-item
+                  class="Title-Commerce"
+                  label="Commerce"
+                  default-opened>
+                  
 
-              <q-item-section>Settings</q-item-section>
-            </q-item>
+                    <q-card class="Card-Commerce" >
+                      
+                          <q-card-section >
+                      
+                            <!-- Border Line Below Commerce -->
+                            <div class="Line-3"></div>
+                            
 
-        </div>
-        <!-- COLLAPSE COMMERCE SECTION -->
-            
-          </q-card-section>
-        </q-card>
-      </q-expansion-item>
+                            <!-- Commerce Menu Items -->
+                            <div class="Commerce">
+
+                                <!-- Refferrals Menu-->
+                                <q-item to="/referrals" clickable v-ripple>
+                                  <q-item-section avatar>
+                                    <q-icon name="group_work" />
+                                  </q-item-section>
+                                  <q-item-section>Referrals</q-item-section>
+                                </q-item>
 
 
-        
-        
-      </q-list>
-      <!-- COLLAPSE SIDE BAR -->
+                                <!-- Audit Logs Menu-->
+                                <q-item to="/audit log" clickable v-ripple>
+                                  <q-item-section avatar>
+                                    <q-icon name="visibility" />
+                                  </q-item-section>
+                                  <q-item-section>Audit logs</q-item-section>
+                                </q-item>
 
-    </q-drawer>
-    <!-- COLLAPSE DRAWER -->
 
-    <q-page-container>
-      <router-view />
-    </q-page-container>
+                                <!-- Settings Menu-->
+                                <q-item to="/setting" clickable v-ripple>
+                                  <q-item-section avatar>
+                                    <q-icon name="settings" />
+                                  </q-item-section>
+                                  <q-item-section>Settings</q-item-section>
+                                </q-item>
+
+                            </div>
+                            
+                          </q-card-section>
+
+                    </q-card>
+                    
+                </q-expansion-item>
+                <!-- COLLAPSE COMMERCE SECTION -->
+
+                
+              </q-list>
+              <!-- COLLAPSE SIDE BAR -->
+
+            </q-drawer>
+            <!-- COLLAPSE DRAWER -->
+
+      <q-page-container>
+        <router-view />
+      </q-page-container>
   </q-layout>
 </template>
 
@@ -183,9 +188,9 @@ export default {
   data () {
     return {
       leftDrawerOpen: false,
-      essentialLinks: linksData
+      essentialLinks: linksData,
+      tab: 'images'
     }
   }
 }
 </script>
-
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,9 +1,6 @@
 <template>
   <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-full.svg"
-    >
+    <h1>This Is a test</h1>
   </q-page>
 </template>
 
