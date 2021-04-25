da63d2eceb6a99a887070294d263a4bd564735bc
zecollokaris <collo.kariss@gmail.com>
2021-04-25 22:25:52 +0300

    added payment issues chart

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -57,6 +57,9 @@
 
           <div id="Success-Rate">FusionCharts will render here</div>
 
+          <div id="Payment-Issues-Chart">FusionCharts will render here</div>
+
+
       </div>
   </div>
 
@@ -68,7 +71,7 @@
 
 <script>
 
-// Include Dependencies
+// Dependencies
 import Vue from 'vue';
 import VueFusionCharts from 'vue-fusioncharts';
 import FusionCharts from 'fusioncharts';
@@ -77,6 +80,49 @@ import FusionTheme from 'fusioncharts/themes/fusioncharts.theme.fusion';
 
 Vue.use(VueFusionCharts, FusionCharts, Column2D, FusionTheme);
 
+
+
+
+
+// Last 30 Days Chart Data
+// A JSON Object To Store The Chart Configurations
+const chartData = [
+  {
+    label: "Venezuela",
+    value: "290"
+  },
+  {
+    label: "Saudi",
+    value: "260"
+  },
+  {
+    label: "Canada",
+    value: "180"
+  },
+  {
+    label: "Iran",
+    value: "140"
+  },
+  {
+    label: "Russia",
+    value: "115"
+  },
+  {
+    label: "UAE",
+    value: "100"
+  },
+  {
+    label: "US",
+    value: "30"
+  },
+  {
+    label: "China",
+    value: "30"
+  }
+];
+
+
+// Success Rate Chart Configurations
 FusionCharts.ready(function() {
   var revenueChart = new FusionCharts({
     type: 'doughnut2d',
@@ -121,46 +167,205 @@ FusionCharts.ready(function() {
 });
 
 
+// Payment Issues Chart Configurations
+FusionCharts.ready(function() {
+  var salesAnlysisChart = new FusionCharts({
+    type: 'mscombi2d',
+    renderAt: 'Payment-Issues-Chart',
+    width: '700',
+    height: '400',
+    dataFormat: 'json',
+    dataSource: {
+      "chart": {
+        "caption": "Harry's SuperMart",
+        "subCaption": "Sales analysis of last year",
+        "xAxisname": "Month",
+        "yAxisName": "Amount (In USD)",
+        "numberPrefix": "$",
+        "divlineColor": "#999999",
+        "divLineIsDashed": "1",
+        "divLineDashLen": "1",
+        "divLineGapLen": "1",
+        "toolTipColor": "#ffffff",
+        "toolTipBorderThickness": "0",
+        "toolTipBgColor": "#000000",
+        "toolTipBgAlpha": "80",
+        "toolTipBorderRadius": "2",
+        "toolTipPadding": "5",
+        "theme": "fusion"
+      },
+      "categories": [{
+        "category": [{
+            "label": "Jan"
+          },
+          {
+            "label": "Feb"
+          },
+          {
+            "label": "Mar"
+          },
+          {
+            "label": "Apr"
+          },
+          {
+            "label": "May"
+          },
+          {
+            "label": "Jun"
+          },
+          {
+            "label": "Jul"
+          },
+          {
+            "label": "Aug"
+          },
+          {
+            "label": "Sep"
+          },
+          {
+            "label": "Oct"
+          },
+          {
+            "label": "Nov"
+          },
+          {
+            "label": "Dec"
+          }
+        ]
+      }],
+      "dataset": [{
+          "seriesName": "Actual Revenue",
+          "showValues": "1",
+          "data": [{
+              "value": "16000"
+            },
+            {
+              "value": "20000"
+            },
+            {
+              "value": "18000"
+            },
+            {
+              "value": "19000"
+            },
+            {
+              "value": "15000"
+            },
+            {
+              "value": "21000"
+            },
+            {
+              "value": "16000"
+            },
+            {
+              "value": "20000"
+            },
+            {
+              "value": "17000"
+            },
+            {
+              "value": "25000"
+            },
+            {
+              "value": "19000"
+            },
+            {
+              "value": "23000"
+            }
+          ]
+        },
+        {
+          "seriesName": "Projected Revenue",
+          "renderAs": "line",
+          "data": [{
+              "value": "15000"
+            },
+            {
+              "value": "16000"
+            },
+            {
+              "value": "17000"
+            },
+            {
+              "value": "18000"
+            },
+            {
+              "value": "19000"
+            },
+            {
+              "value": "19000"
+            },
+            {
+              "value": "19000"
+            },
+            {
+              "value": "19000"
+            },
+            {
+              "value": "20000"
+            },
+            {
+              "value": "21000"
+            },
+            {
+              "value": "22000"
+            },
+            {
+              "value": "23000"
+            }
+          ]
+        },
+        {
+          "seriesName": "Profit",
+          "renderAs": "area",
+          "data": [{
+              "value": "4000"
+            },
+            {
+              "value": "5000"
+            },
+            {
+              "value": "3000"
+            },
+            {
+              "value": "4000"
+            },
+            {
+              "value": "1000"
+            },
+            {
+              "value": "7000"
+            },
+            {
+              "value": "1000"
+            },
+            {
+              "value": "4000"
+            },
+            {
+              "value": "1000"
+            },
+            {
+              "value": "8000"
+            },
+            {
+              "value": "2000"
+            },
+            {
+              "value": "7000"
+            }
+          ]
+        }
+      ]
+    }
+  }).render();
+});
 
 
-const chartData = [
-  {
-    label: "Venezuela",
-    value: "290"
-  },
-  {
-    label: "Saudi",
-    value: "260"
-  },
-  {
-    label: "Canada",
-    value: "180"
-  },
-  {
-    label: "Iran",
-    value: "140"
-  },
-  {
-    label: "Russia",
-    value: "115"
-  },
-  {
-    label: "UAE",
-    value: "100"
-  },
-  {
-    label: "US",
-    value: "30"
-  },
-  {
-    label: "China",
-    value: "30"
-  }
-];
 
 
 
-// STEP 3: Configure your chart
+// Chart Configurations
 const dataSource = {
   chart: {
     caption: "Countries With Most Oil Reserves [2017-18]",
